f05b79e79eefde018df95a323e55c04476ba7e47
ryowa03 <kamijyou76@yahoo.co.jp>
2023-11-24 19:47:34 +0900

    アップデート

---
+++ js/data.js
@@ -0,0 +1,47 @@
+
+    
+    
+    
+    
+    
+    
+    
+     //ランダムに質問事項を形成 
+    $(document).ready(function() {
+        // 質問を表示する関数
+        function showRandomQuestion() {
+            const questions = [
+                "部署の雰囲気について",
+                "残業のリアルは？",
+                "昇給どんな感じ？",
+                "あなたの今の部署での目標は？",
+            
+                "１週間前とかでも有給取りやすい？"
+                // 追加の質問を必要に応じてここに追加
+            ];
+
+            const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+            $('#text').val(randomQuestion);
+        }
+
+        // 本文フォームに特定の値を入力するとランダムに質問を表示
+        $('#text').on('input', function () {
+            const inputValue = $(this).val();
+            const triggerValue = 'おだ';
+
+            if (inputValue.trim() === triggerValue) {
+                showRandomQuestion();
+            }
+        });
+    });
+
+
+
+
+
+
+    //クリアボタンでテキストを削除
+    $('#clear').on('click',function(){
+        $('#output').empty();  
+        });
+        
